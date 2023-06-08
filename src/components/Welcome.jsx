5a14b14c4740fde6d01a7300e27995eac0913c7e
and1kmpo <acampolpez@gmail.com>
2023-06-08 00:36:24 -0500

    decomposition app.jsx

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <div className="flex flex-col justify-center items-center gap-6 m-5">
+        <h1 className="text-4xl font-bold text-center">Your favorite comic book store</h1>
+        <p className="text-base text-center">From classics to novelties, we have everything you need to immerse yourself in your favorite universes. Explore our catalog and live the adventure of your life.</p>
+        <button className="bg-primary w-80 h-16 rounded-full">Get Started</button>
+      </div>          
+    )
+}
