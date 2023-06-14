2251ef82af8184cc2c28a4548e4cff7b69739418
and1kmpo <acampolpez@gmail.com>
2023-06-14 00:35:14 -0500

    Dinamic data Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,21 +1,48 @@
-export default function Carousel({character_photo, cover_photo, name, description}) {
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+import Arrow from './Arrow';
+import apiUrl from '../apiUrl';
+
+export default function Carousel() {
   const d_left = "M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z";
   const d_right = "M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z";
+  //let counter = 0;
+  const [categories,setCategories]  = useState([]);
+  
+  useEffect(
+    () => {
+      axios(apiUrl+'/categories')
+        .then(res=>{
+        /* console.log(res.data.response) */
+          setCategories(res.data.response);
+        }).catch(err =>console.log(err))
+      },[]
+);
+
+
+  const [counter, setCounter] = useState(0);
+  const next = ()=>{
+    //counter = counter + 1;
+    setCounter(counter+1);
+    console.log(counter);
+  }
+  const prev = ()=>{
+    //counter = counter - 1;
+    setCounter(counter -1);
+    console.log(counter);
+}
+console.log(categories);
   return (
     <div className="hidden md:flex items-center justify-center mt-5 px-10 md:justify-evenly md:h-96 md:w-full bg-white">  
         <div className="bg-primary w-[90%] h-[60%] rounded-lg flex justify-evenly items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 cursor-pointer" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={d_left} />
-            </svg>
-            <img className="ml-20 h-64 self-end" src={character_photo} alt="imagen manga" />
-            <img className="h-56 mb-12 self-end rounded-lg" src={cover_photo} alt="cover manga" />
-            <div className="text-lg px-32 text-black sm:w-10/12 xl:w-6/12">
-                <h3 className="text-2xl text-white pb-4">{name}</h3>
-                <p className="text-xs text-white xl:text-sm">{description}</p>
+            <Arrow d={d_left } onClick={prev}/>
+            <img className="ml-20 h-64 self-end" src={categories[0]?.character_photo} alt="imagen manga" />
+            <img className="h-56 mb-12 self-end rounded-lg" src={categories[0]?.cover_photo} alt="cover manga" />
+            <div className="text- px-32 text-black sm:w-10/12 xl:w-6/12">
+                <h3 className="text-3xl text-white pb-2 capitalize">{categories[0]?.name}</h3>
+                <p className="text-xs text-white xl:text-sm">{categories[0]?.description}</p>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 cursor-pointer" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={d_right} />
-            </svg>
+            <Arrow d={d_right} onClick={next}/>
         </div>
 </div>
   )
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,7 +5,7 @@ export default function Index({data}) {
       return (
         <main>
             <Welcome/>
-            <Carousel name={data[0].title} character_photo={data[0].character_photo} cover_photo={data[0].cover_photo} description={data[0].description}/>
+            <Carousel name={data[0].name} character_photo={data[0].character_photo} cover_photo={data[0].cover_photo} description={data[0].description}/>
       </main>
   )
 }
