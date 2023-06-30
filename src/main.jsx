7bee847b2121030529b1a0d86aa81382b56dfaf6
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-06-29 22:16:23 -0300

    M-01 (#14)
    * minga-01
    
    * dix
    
    * navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './pages/router'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
