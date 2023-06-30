1dbe93d6d19a55c9513c64529fd266216fe1aa36
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-06-29 20:17:13 -0500

    Modified view - Width components CiaForm & ChapterForm & MangaForm (#15)
    - Se ajusta el ancho de los Input en los componentes CiaForm - MangaForm - ChapterForm .

--- src/pages/CiaForm.jsx
+++ src/pages/CiaForm.jsx
@@ -1,10 +1,12 @@
 export default function CiaForm() {
+    const img_company = 'src/assets/new_company.svg'
     return (
         <>
             <main className='flex w-full min-h-screen items-center justify-center overflow-x-hidden mb-6 font-roboto'>
                 <div className='flex flex-col mt-[10%] md:mt-[20%] lg:mt-[5%] justify-center items-center w-screen lg:w-[50%] lg:left-[50%]'>
-                    <h1 className='text-4xl text-center'>New Company</h1>
-                    <div className='flex flex-col mt-8 gap-6'>
+                    <h1 className='mt-8 text-4xl text-center'>New Company</h1>
+                    <img src={img_company} alt="company" className="mt-8 h-[105px] w-[100px]" />
+                    <div className='flex flex-col w-72 mt-5 gap-6'>
                         <input className="px-4 h-[50px] border-b hover:rounded-lg hover:border-rounded hover:border-purple" type="text" name="name" id="name" placeholder="Name" required />
                         <input className="px-4 h-[50px] border-b hover:rounded-lg hover:border-rounded hover:border-purple" type="text" name="website" id="website" placeholder="Website" required />
                         <input className="px-4 h-[50px] border-b hover:rounded-lg hover:border-rounded hover:border-purple" type="text" name="url" id="url" placeholder="URL Profile Image" required />
