dbb7532c6010391c4a5e9e8d03f96ce08743b23b
stivosp <stiven1432@gmail.com>
2023-07-10 16:33:52 -0500

    authorForm

--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,7 @@ import ChapterForm from './pages/ChapterForm';
 import SignIn from './pages/Signin';
 import Register from './pages/Register';
 import CiaForm from './pages/CiaForm';
+import AuthorForm from './pages/AuthorForm';
 
 const router = createBrowserRouter([{
     path: '/', // con una ruta renderizo un componente de tipo layout
@@ -17,8 +18,8 @@ const router = createBrowserRouter([{
         { path: '/manga-form', element: <MangaForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 0 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
         { path: 'manga/:manga_id/chapter-form', element: <ChapterForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 0 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
         { path: '/cia-form', element: <CiaForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 1 || JSON.parse(localStorage.getItem('user')).role === 2 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
-        //Falta crear AuthorForm y NotAllowed
-        { path: '/author-form', element: <CiaForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 1 || JSON.parse(localStorage.getItem('user')).role === 2 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') }
+        //Falta crear NotAllowed
+        { path: '/author-form', element: <AuthorForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 1 || JSON.parse(localStorage.getItem('user')).role === 2 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') }
     ]
 }])
 
