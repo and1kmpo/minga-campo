3b538a648a865e7b7af8f24e76272be67f7f9a1c
and1kmpo <acampolpez@gmail.com>
2023-07-27 01:18:53 -0500

    add checkbox categories

--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -5,13 +5,15 @@ import headers from "../utils/headers"
 import CardManga from "../components/CardManga"
 import { useSelector, useDispatch } from "react-redux"
 import manga_action from "../store/actions/mangas"
-const { save_title } = manga_action
+const { save_title, save_category } = manga_action
 
 export default function Mangas() {
     const store = useSelector(store => store)
     const dispatch = useDispatch()
     console.log(store)
     const [title, setTitle] = useState("")//Title debe ser una variable globlal para que cuando el componente se desmonte no se pierda el filtro por title
+    const [category, setCategory] = useState([])
+    const [selectedCategories, setSelectedCategories] = useState([]);
     const [mangas, setMangas] = useState([])
     const [prev, setPrev] = useState(null)
     const [next, setNext] = useState(null)
@@ -33,9 +35,33 @@ export default function Mangas() {
             });
         }, [store.mangas.text]);
 
+    useEffect(
+        () => {
+            axios(apiUrl + "/categories", headers()).then(res => {
+                setCategory(res.data.response)
+            }).catch((err) => {
+                if (err.response.status === 404) {
+                    setMangas([])
+                    setNotFound(true)
+                } else {
+                    console.error(err)
+                }
+            });
+        }, []);
+
+    const handleCategoryChange = (event) => {
+        const categoryId = event.target.name;
+        setSelectedCategories((prevSelected) => {
+            if (prevSelected.includes(categoryId)) {
+                return prevSelected.filter((id) => id !== categoryId);
+            } else {
+                return [...prevSelected, categoryId];
+            }
+        });
+    };
     return (
         <>
-            {/* <input type="text" placeholder="Search..." className="mt-[100px] ml-[100px]" onChange={(event) => setTitle(event.target.value)} /> Así se hace con estados locales*/}
+
             <input
                 type="text"
                 placeholder="Search..."
@@ -43,11 +69,19 @@ export default function Mangas() {
                 onChange={(event) => dispatch(save_title({ title: event.target.value }))}
                 defaultValue={(store.mangas.text)} />
 
+            <div className="w-100 text-center">
+                {category.map((cat) => (
+                    <label key={cat._id} className="m-4">
+                        <input type="checkbox" name={cat._id} id="" /> {cat.name}
+                    </label>
+                ))}
+            </div>
+
             <div className="flex items-center justify-center min-h-[400px]">
                 {notFound ? (<p className="w-100 text-center">No results found matching your search.</p>) : (mangas.map(each => <CardManga key={each._id} title={each.title} />))}
 
             </div>
-            <div>
+            <div className="w-100 text-center">
                 {prev && <input type="button" value={"previus page"} />}
                 {next && <input type="button" value={"next page"} />}
             </div>
