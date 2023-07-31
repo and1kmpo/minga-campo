933d1d09b09212394b41dbda3d06456c927b6d3a
and1kmpo <acampolpez@gmail.com>
2023-07-31 18:39:12 -0500

    syles mangas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {
-        fontFamily: {
-          poppins: ['Poppins', 'sans-serif'],
-          roboto: ['Roboto', 'sans-serif'],
-        },
-        colors: {
-            primary: "#4338CA", purple:'#4338CA' 
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#4338CA",
+        purple: "#4338CA",
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
