4ef085cc5714c3bff0151711fe223b90eaa2b73f
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-08-01 18:20:11 -0500

    m07 store (#38)
    * Add favicon
    
    * base for prev-next buttons
    
    * Use base for manga in store
    
    * Update useEffect para evitar re fetchear
    
    * symbol missing

--- src/pages/MangaDetail.jsx
+++ src/pages/MangaDetail.jsx
@@ -1,31 +1,42 @@
 import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import axios from "axios"
 import apiUrl from "../apiUrl"
 import headers from "../utils/headers"
 import CardMangaChapter from "../components/CardMangaChapter"
+import manga_actions from "../store/actions/mangas";
 
 export default function MangaDetail() {
+    let manga = useSelector(store => store.mangas.manga)
+    // console.log(manga)
+    let dispatch = useDispatch()
+    let { save_title, save_manga } = manga_actions
+
     let { manga_id, page } = useParams()
-    let [ manga, setManga ] = useState("")
+    // let [ manga, setManga ] = useState("") // asi se guarda con estados locales, se reemplazarán por estados globales para optimizar el rendimiento de la aplicación
     let [ chapters, setChapters ] = useState([])
     let [ prev, setPrev ] = useState(null)
     let [ next, setNext ] = useState(null)
     let [ openTab, setOpenTab ] = useState(1)
-    // console.log("manga_id", manga_id)
 
     useEffect(
         () => {
-            // axios necesita la ruta del backend
-            axios(apiUrl+'/mangas/'+manga_id, headers())
+            if (manga?._id !== manga_id) {
+            // if (!manga) { // este condicional funciona pero cuando cambie de manga no se volverá a re fetchear el nuevo manga
+                // axios necesita la ruta del backend
+                axios(apiUrl+'/mangas/'+manga_id, headers())
                 .then( res => {
                     // datos dinamicos a renderizar
-                    // console.log(res.data.response)
-                    setManga(res.data.response)
+                    // asi se guarda con estados locales
+                    // setManga(res.data.response)
+                    console.log(res.data.response)
+                    dispatch(save_manga({manga: res.data.response}))
                 })
                 .catch(
                     err => console.log(err)
                 )
+            }
         },
         []
     )
@@ -35,7 +46,6 @@ export default function MangaDetail() {
             // axios necesita la ruta del backend
             axios(apiUrl+`/chapters?manga_id=${manga_id}`, headers())
                 .then( res => {
-                        console.log(res.data)
                         setChapters(res.data.response)
                         setPrev(res.data.prev_page)
                         setNext(res.data.next_page)
@@ -52,15 +62,15 @@ export default function MangaDetail() {
 
             <div id="div-title" className="flex flex-col items-center px-4 pt-2">
                 <img className="rounded-xl w-full h-[40vh] lg:h-[60vh] object-cover object-top"
-                     src={manga.cover_photo}
+                     src={manga?.cover_photo}
                      alt="cover_photo"/>
-                <h1 className={'text-[40px] font-normal ' + (openTab === 1 ? "block" : "hidden")}>{manga.title}</h1>
+                <h1 className={'text-[40px] font-normal ' + (openTab === 1 ? "block" : "hidden")}>{manga?.title}</h1>
                 <div className={"w-full flex justify-between " + (openTab === 1 ? "block" : "hidden")}>
-                    <span style={{color: manga.category_id?.color}} className="capitalize text-[12px] font-medium rounded-xl py-2 px-4">
-                        {manga.category_id?.name}
+                    <span style={{color: manga?.category_id?.color}} className="capitalize text-[12px] font-medium rounded-xl py-2 px-4">
+                        {manga?.category_id?.name}
                     </span>
                     <span className="font-poppins capitalize text-[20px] px-4 font-medium text-[#9D9D9D]">
-                        {manga.author_id?.name}
+                        {manga?.author_id?.name}
                     </span>
                 </div>
                 <div className={openTab === 2 ? "block" : "hidden"}>
@@ -127,13 +137,19 @@ export default function MangaDetail() {
                     <div className="">
 
                         <div className={openTab === 1 ? "block" : "hidden"} id="link1">
-                            <p> {manga.description} </p>
+                            <p> {manga?.description} </p>
                         </div>
 
                         <div className={"flex flex-col gap-10 pt-4 " + (openTab === 2 ? "block" : "hidden")} id="link2">
                             {chapters.map(each =>
                                 <CardMangaChapter key={each.title} title={each.title} cover_photo={each.cover_photo} pages={each.pages.length} />
                             )}
+                            <div className="flex flex-col items-center text-[12px] font-semibold">
+                                <div className="flex divide-x text-slate-700 divide-slate-400 border shadow rounded-md bg-white p-1">
+                                    { prev && <input type="button" value={"Prev"} /> }
+                                    { next && <input type="button" value={"Next"} /> }
+                                </div>
+                            </div>
                         </div>
 
                     </div>
