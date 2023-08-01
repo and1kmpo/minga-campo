ce05f8c20a4c04e0b8cded0a2f4f9503439adbe8
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-07-31 23:16:54 -0500

    Router Conflict

--- src/router.jsx
+++ src/router.jsx
@@ -1,37 +1,81 @@
-import { createBrowserRouter, redirect } from 'react-router-dom';
-import Main from './layouts/Main';
-import Index from './pages/Index';
-import MangaForm from './pages/MangaForm';
-import ChapterForm from './pages/ChapterForm';
-import SignIn from './pages/Signin';
-import Register from './pages/Register';
-import CiaForm from './pages/CiaForm';
-import AuthorForm from './pages/AuthorForm';
-import Mangas from './pages/Mangas';
-import MangaDetail from './pages/MangaDetail';
-import Author from './pages/Author';
-import Chapters from './pages/chapters';
+import { createBrowserRouter, redirect } from "react-router-dom";
+import Main from "./layouts/Main";
+import Index from "./pages/Index";
+import MangaForm from "./pages/MangaForm";
+import ChapterForm from "./pages/ChapterForm";
+import SignIn from "./pages/Signin";
+import Register from "./pages/Register";
+import CiaForm from "./pages/CiaForm";
+import AuthorForm from "./pages/AuthorForm";
+import Mangas from "./pages/Mangas";
+import MangaDetail from "./pages/MangaDetail";
+import Chapters from "./pages/chapters";
 
-
-const router = createBrowserRouter([{
-    path: '/', // con una ruta renderizo un componente de tipo layout
+const router = createBrowserRouter([
+  {
+    path: "/", // con una ruta renderizo un componente de tipo layout
     element: <Main />,
-    children: [ // rutas hijas
-        { path: '/', element: <Index /> },
-        { path: '/register', element: <Register />, loader: () => localStorage.getItem('user') && redirect('/') },
-        { path: '/signin', element: <SignIn />, loader: () => localStorage.getItem('user') && redirect('/') },
-        { path: '/manga-form', element: <MangaForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 0 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
-        { path: '/manga/:manga_id/:page', element: <MangaDetail /> },
-        { path: 'manga/:manga_id/chapter-form', element: <ChapterForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 0 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
-        { path: '/cia-form', element: <CiaForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 1 || JSON.parse(localStorage.getItem('user')).role === 2 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
-        //Falta configurar NotAllow
-        { path: '/author-form', element: <AuthorForm />, loader: () => (JSON.parse(localStorage.getItem('user')).role === 1 || JSON.parse(localStorage.getItem('user')).role === 2 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') },
-        { path: '/me', element: <Author></Author>},
-        { path: '/mangas/:page', element: <Mangas /> }
-    ]
-        { path: '/mangas/:page', element: <Mangas /> },
-        { path: '/chapter/:id/:page', element: <Chapters/>, loader: () => (JSON.parse(localStorage.getItem('user')).role === 0 || JSON.parse(localStorage.getItem('user')).role === 3) && redirect('/') }
-   ]
-}])
+    children: [
+      // rutas hijas
+      { path: "/", element: <Index /> },
+      {
+        path: "/register",
+        element: <Register />,
+        loader: () => localStorage.getItem("user") && redirect("/"),
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+        loader: () => localStorage.getItem("user") && redirect("/"),
+      },
+      {
+        path: "/manga-form",
+        element: <MangaForm />,
+        loader: () =>
+          (JSON.parse(localStorage.getItem("user")).role === 0 ||
+            JSON.parse(localStorage.getItem("user")).role === 3) &&
+          redirect("/"),
+      },
+      { path: "/manga/:manga_id/:page", element: <MangaDetail /> },
+      {
+        path: "manga/:manga_id/chapter-form",
+        element: <ChapterForm />,
+        loader: () =>
+          (JSON.parse(localStorage.getItem("user")).role === 0 ||
+            JSON.parse(localStorage.getItem("user")).role === 3) &&
+          redirect("/"),
+      },
+      {
+        path: "/cia-form",
+        element: <CiaForm />,
+        loader: () =>
+          (JSON.parse(localStorage.getItem("user")).role === 1 ||
+            JSON.parse(localStorage.getItem("user")).role === 2 ||
+            JSON.parse(localStorage.getItem("user")).role === 3) &&
+          redirect("/"),
+      },
+      //Falta configurar NotAllow
+      {
+        path: "/author-form",
+        element: <AuthorForm />,
+        loader: () =>
+          (JSON.parse(localStorage.getItem("user")).role === 1 ||
+            JSON.parse(localStorage.getItem("user")).role === 2 ||
+            JSON.parse(localStorage.getItem("user")).role === 3) &&
+          redirect("/"),
+      },
+      { path: "/mangas/:page", element: <Mangas /> },
+      { path: "/mangas/:page", element: <Mangas /> },
+      {
+        path: "/chapter/:id/:page",
+        element: <Chapters />,
+        loader: () =>
+          (JSON.parse(localStorage.getItem("user")).role === 0 ||
+            JSON.parse(localStorage.getItem("user")).role === 3) &&
+          redirect("/"),
+      },
+    ],
+  }
+])
 
 export default router;
