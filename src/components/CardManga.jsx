81915e541199aea02d3a168c0b802e0490a45a9e
and1kmpo <acampolpez@gmail.com>
2023-08-05 00:07:37 -0500

    finishing view mangas

--- src/components/CardManga.jsx
+++ src/components/CardManga.jsx
@@ -1,15 +1,15 @@
 export default function CardManga({ title, category, imageSrc, color }) {
     return (
-        <div className="w-[300px] h-[150px] flex items-center m-10 rounded-xl border-l-8 font-poppins shadow-xl" style={{ borderColor: color }}>
+        <div className="min-w-[400px] h-[200px] bg-white flex items-center mt-6 rounded-xl border-l-8 font-poppins shadow-xl" style={{ borderColor: color }}>
             <div className="flex flex-col p-4" >
-                <h1 className="text-black text-xl font-bold">{title}</h1>
-                <p className="text-lg mb-4 font-semibold" style={{ color: color }}>{category}</p>
+                <h1 className="text-black text-xl font-medium">{title}</h1>
+                <p className="text-lg mb-4 capitalize font-medium" style={{ color: color }}>{category}</p>
             </div>
             <div className="flex-grow"></div>
             <img
                 src={imageSrc}
                 alt={title}
-                className="w-[150px] h-[150px] object-cover rounded-r-xl rounded-[50%_0px_0px_50%]"
+                className="min-w-[150px] max-w-[125px] h-[200px] object-cover rounded-r-xl rounded-[50%_0px_0px_50%]"
             />
         </div>
     );
