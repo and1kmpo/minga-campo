1318f89fc6b1ea7d1d04ecbd5b27b3555b59beb8
Natalia <125935088+Natalia001-bz@users.noreply.github.com>
2023-08-08 22:29:56 -0500

    Fix front (#48)
    * Fix styles home
    
    * Update anchor-to to div
    
    * Fix view home - margin in cover image
    
    * fix-front
    
    ---------
    
    Co-authored-by: Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -26,39 +26,39 @@ export default function Carousel() {
     counter !== 0 ? modify(counter - 1) : modify(categories.length - 1);
 
   return (
-    <div className="flex justify-between  hidden md:block mb-10  ">
-      <div
-        className="h-[265px]  rounded-md flex items-center relative ml-10 mr-10 mt-10"
-        style={{ backgroundColor: categories[counter]?.color }}
-      >
-        <div className="ml-0 md_ml-4 ">
-          <Arrow d={d_left} onClick={prev} />
-        </div>
-        <img
-          className=" ml-[8%] h-[100%] w-[15%] absolute"
-          src={categories[counter]?.character_photo}
-          alt="imagen1"
-        ></img>
-        <img
-          className=" ml-[28%] lg:ml-[30%] -mt-[40px] h-[284px] w-[189px] absolute"
-          src={categories[counter]?.cover_photo}
-          alt="imagen2"
-        ></img>
+    <div className="hidden lg:flex px-14 py-7 xl:py-10">
+      <div className="h-[165px] xl:h-[245px] w-full rounded-md flex items-center"
+           style={{ backgroundColor: categories[counter]?.color }}>
 
-        <div className="flex flex-col item-start justify-center w-[40%] lg:w-[45%] h-auto  text-gray-100 absolute ml-[49%]">
-          <h3 className="font-Roboto font-bold text-2xl xl:text-3xl ">
+        <div className="pl-6" onClick={prev}>
+          <Arrow d={d_left} />
+        </div>
+
+        <div className="w-[50%] h-full flex justify-evenly">
+          <img
+            className="w-[175px] xl:w-[270px] h-[190px] xl:h-[286px] mr-8 self-end"
+            src={categories[counter]?.character_photo}
+            alt="character"></img>
+          <img
+            className="w-[132px] xl:w-[180px] h-[170px] xl:h-[260px] -mt-[20px] xl:-mt-[30px] rounded-sm"
+            src={categories[counter]?.cover_photo}
+            alt="cover"></img>
+        </div>
+
+        <div className=" w-[50%] flex flex-col justify-center font-Roboto pl-10 pr-20"
+             style={{ color: categories[counter]?.hover }}>
+          <h3 className="text-[24px] font-medium">
             {categories[counter]?.name.toUpperCase()}
           </h3>
-          <p
-            className="text-[20px] font-Roboto text-sm xl:text-xl font-normal "
-            style={{ color: categories[counter]?.hover }}
-          >
+          <p className="text-[14px] xl:text-xl font-normal leading-[14.33px]">
             {categories[counter]?.description}
           </p>
         </div>
-        <div className="flex justify-between ml-[89%]">
-          <Arrow d={d_right} onClick={next} />
+
+        <div className="pr-6" onClick={next}>
+          <Arrow d={d_right} />
         </div>
+
       </div>
     </div>
   );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,9 +3,9 @@ import Welcome from "../components/Welcome";
 
 export default function Index() {
   return (
-    <>
+    <div className="flex flex-col h-screen font-roboto mb-10">
       <Welcome />
       <Carousel />
-    </>
-  );
+    </div>
+  )
 }
