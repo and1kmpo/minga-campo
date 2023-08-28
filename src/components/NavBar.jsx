2f2a2c51c6b25fbbfdf293b46b67386cff63d860
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-08-28 16:15:49 -0500

    Updating color white for close button in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -172,7 +172,7 @@ export default function NavBar() {
               </div>
             </div>
             <button onClick={() => setShowMenu(false)}>
-              <RiCloseFill className="text-3xl" />
+              <RiCloseFill className="text-3xl text-white" />
             </button>
           </div>
           <div className="pt-5">
