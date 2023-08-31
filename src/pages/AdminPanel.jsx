1b3b7a79799da4b0fe12cb723caa1dbaa82e6072
and1kmpo <acampolpez@gmail.com>
2023-08-31 01:11:51 -0500

    fix paragraph Panel margin and size text

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -13,10 +13,10 @@ const AdminPanel = () => {
                 className='w-full h-[369px] lg:h-[650px] bg-cover bg-center flex flex-col items-center justify-center'
                 style={{ backgroundImage: `url(${bgAdminPanel})` }}
             >
-                <p className="-mt-[9%] text-[64px] text-white font-roboto font-medium">Panel</p>
+                <p className="-mt-[9%] text-[48px] md:text-[64px] text-white font-roboto font-medium md:-mt-[6%]">Panel</p>
             </div>
-            <div className="relative -top-[174px] bg-white p-[20px] lg:p-[50px] w-[90%] flex flex-col justify-center items-center rounded-2xl">
-                <p className="text-primary text-[32px] font-bold border-b-2 border-primary mb-6">Entities</p>
+            <div className="relative -top-[174px] bg-white p-[20px] lg:p-[50px] w-[90%] md:w-[80%] sm:w-full flex flex-col justify-center items-center rounded-2xl">
+                <p className="text-primary text-[24px] md:text-[32px] font-bold border-b-2 border-primary mb-6">Entities</p>
                 <ToggleSwitchAdmin showCompanies={showCompanies} setShowCompanies={setShowCompanies} />
                 {showCompanies ? (<AdminRoleCompanies />) : (<AdminRoleAuthors />)}
             </div>
